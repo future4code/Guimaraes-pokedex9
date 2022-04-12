d55831c9dcdd3d7fdd44e2a02e5f6898013acca7
jucelia xavier da silva <juceliaxavier29@gmail.com>
2022-04-12 20:53:03 -0300

    adicionei a pagina home e router

---
+++ pokedex9/src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import axios from "axios";
+
+
+
+const HomePage = () => {
+
+    return (
+        <div>
+            <h1>HomePage</h1>
+            <button>Pagina</button>
+        </div>
+    );
+}
+
+
+export default HomePage;
