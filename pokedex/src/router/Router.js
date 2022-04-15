50e31a3415af0728ba3779c354bbc29c4b9fc37b
jucelia xavier da silva <juceliaxavier29@gmail.com>
2022-04-14 21:10:36 -0300

    atualização

--- pokedex/src/router/Router.js
+++ pokedex/src/router/Router.js
@@ -1,3 +1,17 @@
 import React from "react";
 import HomePage from "../pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+const Router = () => {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path={"/"} element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  };
+  
+  export default Router;
