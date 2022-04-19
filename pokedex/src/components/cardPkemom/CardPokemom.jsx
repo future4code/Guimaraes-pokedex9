34e810ab8707ec6db78dc16e43fe729f79ccb856
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-04-19 20:19:42 -0300

    card pokemom

--- pokedex/src/components/cardPkemom/CardPokemom.jsx
+++ pokedex/src/components/cardPkemom/CardPokemom.jsx
@@ -1,18 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 import"./cardPokemom.css"
 
+const Container=styled.div`
+    height:250px ;
+    
+    padding:10px ;
 
+    img{
+        height:70% ;
+        margin:10px ;
+        
+    }
+    button:hover{
+        background-color: #fff856 ;
+        cursor: pointer ;
+    }
+`
 
 
 
 export default ()=>{
     return(
-        <div className="card-pokemom">
+        <Container className="card-pokemom">
             <img  src="https://cdn.pixabay.com/photo/2016/07/23/13/18/pokemon-1536849_960_720.png" alt="pokemom"/>
             <div>
                 <button>Adicionar Pokemom</button>
                 <button>Ver detalhes</button>
             </div>
-        </div>
+        </Container>
     )
 }
