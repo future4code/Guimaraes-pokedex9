1dec018fe511b08577b945db24fe057cafd5e025
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-04-24 20:01:25 -0300

    botoes header e icones footer

--- pokedex/src/components/header/Header.jsx
+++ pokedex/src/components/header/Header.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./header.css"
 
 
 
-
 export default ()=>{
+    const navigate=useNavigate()
+
+    const voltar=()=>{
+       navigate("/")
+        
+    }
+    const irPokedex=()=>{
+        navigate("/pokedex")
+    }
     return(
         <div className="container">
-            <span> Pokedex</span>
+            <span> <img src="https://o.remove.bg/downloads/5046d4b1-d82b-4cb0-a3c4-b01430b2aab6/pokeapi_256-removebg-preview.png"/></span>
             <h1>lista de Pokemons</h1>
             <div>
-            <button>ir para Pokedex</button>
-            <button>voltar</button>
+            <button onClick={()=>irPokedex()}>ir para Pokedex </button>
+            <button onClick={()=>voltar()}>voltar</button>
             </div>
         </div>
     )
